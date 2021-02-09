1a0841e8dd4bd3d0e80f7955d2cd40176245d4fa
Matt Huszagh <huszaghmatt@gmail.com>
2021-02-09 07:49:33 -0800

    perform recursion on branch node instead of headline

--- org-api.el
+++ org-api.el
@@ -16,34 +16,30 @@
   "Get a flat list of all first-level headlines in a NODE."
   (org-ml-match '(headline) node))
 
-(defun org-api/get-nodes-in-section (section condition)
-  "Get a list of nodes in SECTION matching CONDITION.
-CONDITION is a pattern used with `org-ml-match' that must evaluate to true
-for the node to be included in the list of nodes."
-  (org-ml-match condition section))
-
-(defun org-api/get-nodes-recursive-in-headline (headline nodes condition)
-  "Get all NODES in HEADLINE recursively matching CONDITION.
-CONDITION is a pattern used with `org-ml-match' that must evaluate to true
-for the node to be included in the list of nodes."
-  (let ((section (org-api/get-section headline))
-        (headlines (org-api/get-headlines headline)))
-    (setq nodes
-          (append nodes (org-api/get-nodes-in-section section condition)))
-    (dolist (headline headlines)
+(defun org-api/get-matching-nodes-recursive-in-node (node nodes condition)
+  "Get a flat list of nodes matching CONDITION in NODE.
+NODES is a list of existing matched nodes."
+  (if (org-ml-is-childless node)
+      (append nodes (org-ml-match condition node))
+    (let ((children (org-ml-get-children node)))
       (setq nodes
-            (org-api/get-nodes-recursive-in-headline headline nodes condition)))
-    nodes))
+            (append nodes (org-ml-match condition node)))
+      (dolist (child children)
+        (setq nodes
+              (org-api/get-matching-nodes-recursive-in-node child nodes condition)))
+      nodes)))
 
 (defun org-api/get-nodes-recursive-in-current-buffer (condition)
   "Get a flat list of all nodes in the current buffer matching CONDITION.
 The syntax of CONDITION is the same as that of `pattern' in `org-ml-match'."
   (let* ((buffer-tree (org-ml-parse-this-buffer))
-         (leading-section (org-api/get-section buffer-tree))
-         (nodes (org-api/get-nodes-in-section leading-section condition))
-         (headlines (org-api/get-headlines buffer-tree)))
-    (dolist (headline headlines)
-      (setq nodes (org-api/get-nodes-recursive-in-headline headline nodes condition)))
+         (nodes '()))
+    ;; TODO I'm not actually sure it's possible to have a childless
+    ;; root node.
+    (if (org-ml-is-childless buffer-tree)
+        (setq nodes
+              (append nodes (org-api/get-nodes-in-leaf buffer-tree condition)))
+      (setq nodes (org-api/get-matching-nodes-recursive-in-node buffer-tree nodes condition)))
     nodes))
 
 (defun org-api/map-nodes-recursive-in-current-buffer (fun condition)
